6e0d768acf615b88c3a126d208b905a996f874db
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-01 15:50:38 -0700

    lab complete

---
+++ server/controller.js
@@ -0,0 +1,49 @@
+const houses = require('./db.json')
+let globalID = 4
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => +elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    },
+
+    createHouse: (req, res) => {
+        let newHouse = req.body
+
+        newHouse.id = globalID
+
+        newHouse.price = parseInt(newHouse.price)
+        
+        houses.push(newHouse)
+
+
+        res.status(200).send(houses)
+        globalID++
+        
+    },
+    
+    updateHouse: (req, res) => {
+        let{id} = req.params
+        let {type} = req.body
+
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if(type === "plus") {
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else if(type === 'minus' && houses[index].price === 0) {
+            res.status(400).send('cannot go lower in price')
+        } else if(type === 'minus') {
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else {
+            res.sendStatus(400)
+        }
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 
+const {getHouses, createHouse, updateHouse, deleteHouse} = require('./controller')
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/api/houses', getHouses)
+
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('/api/houses/:id', deleteHouse)
+
+
 const SERVER_PORT = 4004
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running on ${SERVER_PORT}`)
-})
+}) 
